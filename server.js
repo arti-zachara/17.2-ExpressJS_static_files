c94b00b064c57eb49af181b05fae1468124431fc
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-05-27 00:57:55 +0200

    Add 404 handling

--- server.js
+++ server.js
@@ -24,3 +24,10 @@ var server = app.listen(3000, "localhost", function() {
     "Przykładowa aplikacja nasłuchuje na http://" + host + ":" + port
   );
 });
+
+// ------ handling 404
+app.use(function(req, res, next) {
+  res
+    .status(404)
+    .send("Not what you've been looking for but still I love U 3000");
+});
